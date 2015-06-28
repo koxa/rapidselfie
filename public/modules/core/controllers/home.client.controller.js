d5ce3ebf8eaf395e22d2b15c841aa6c4922c534b
Constantine Priemski <konstantyn.pr@gmail.com>
2015-06-28 10:24:06 -0700

    remove call to get list after new selfie added

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -2,73 +2,73 @@
 
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http', '$stateParams',
-	function($scope, Authentication, $http, $stateParams) {
-		// This provides Authentication context.
-		$scope.dataUri = null;
-		$scope.domainName = window.location.origin;
-		$scope.link = null;
-		//$scope.authentication = Authentication;
+    function ($scope, Authentication, $http, $stateParams) {
+        // This provides Authentication context.
+        $scope.dataUri = null;
+        $scope.domainName = window.location.origin;
+        $scope.link = null;
+        //$scope.authentication = Authentication;
 
-		$scope.showWebcam = function() {
-			$scope.dataUri = null;
-			$scope.fromLink = null;
-			window.Webcam.set({
-				width: 320,
-				height: 240,
-				dest_width: 320,
-				dest_height: 240
-			});
-			window.Webcam.attach('#videoCapture');
-		}
+        $scope.showWebcam = function () {
+            $scope.dataUri = null;
+            $scope.fromLink = null;
+            window.Webcam.set({
+                width: 320,
+                height: 240,
+                dest_width: 320,
+                dest_height: 240
+            });
+            window.Webcam.attach('#videoCapture');
+        };
 
-		$scope.listSelfies = function() {
-			$http.get('/selfie')
-				.success(function(selfies) {
-					$scope.selfies = selfies;
-				})
-				.error(function() {
+        $scope.listSelfies = function () {
+            $http.get('/selfie')
+                    .success(function (selfies) {
+                        $scope.selfies = selfies;
+                    })
+                    .error(function () {
 
-				})
-		}
+                    });
+        };
 
-		if ($stateParams.selfieId) {
-			$http.get('/selfie/' + $stateParams.selfieId)
-				.success(function(data) {
-					$scope.dataUri = data.dataUri;
-					$scope.fromLink = true;
-				})
-				.error(function() {
-					$scope.showWebcam();
-				})
-		}
-		else {
-			$scope.showWebcam();
-			$scope.listSelfies();
-		}
+        if ($stateParams.selfieId) {
+            $http.get('/selfie/' + $stateParams.selfieId)
+                    .success(function (data) {
+                        $scope.dataUri = data.dataUri;
+                        $scope.fromLink = true;
+                    })
+                    .error(function () {
+                        $scope.showWebcam();
+                    });
+        }
+        else {
+            $scope.showWebcam();
+            $scope.listSelfies();
+        }
 
-		$scope.snap = function() {
-			window.Webcam.snap(function(data_uri) {
-				$scope.dataUri = data_uri;
-			});
-		}
+        $scope.snap = function () {
+            window.Webcam.snap(function (data_uri) {
+                $scope.dataUri = data_uri;
+            });
+        };
 
-		$scope.delete = function() {
-			$scope.dataUri = null;
-		}
+        $scope.delete = function () {
+            $scope.dataUri = null;
+        };
 
-		$scope.save = function() {
-			$http.post('/selfie', {
-				dataUri: $scope.dataUri
-			}).
-			success(function(data, status, headers, config) {
-				$scope.delete();
-				$scope.selfieId = data._id;
-				$scope.link = $scope.domainName + "/#!/" + $scope.selfieId;
-				$scope.listSelfies();
-			}).
-			error(function(data, status, headers, config) {
-				$scope.delete();
-			});
-		}
-	}
+        $scope.save = function () {
+            $http.post('/selfie', {
+                dataUri: $scope.dataUri
+            }).
+                    success(function (data, status, headers, config) {
+                        $scope.delete();
+                        $scope.selfieId = data._id;
+                        $scope.link = $scope.domainName + "/#!/" + $scope.selfieId;
+                        $scope.selfies.push(data);
+                    }).
+                    error(function (data, status, headers, config) {
+                        $scope.delete();
+                    });
+        }
+    }
 ]);
