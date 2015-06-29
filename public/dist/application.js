8868c74cc64aefa15b13c7e914e61c051607bb5e
Constantine <priemski.k@gmail.com>
2015-06-29 18:32:16 +0000

    up build

--- public/dist/application.js
+++ public/dist/application.js
@@ -202,14 +202,14 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http', '$stateParams',
-    function ($scope, Authentication, $http, $stateParams) {
+    function($scope, Authentication, $http, $stateParams) {
         // This provides Authentication context.
         $scope.dataUri = null;
         $scope.domainName = window.location.origin;
         $scope.link = null;
         //$scope.authentication = Authentication;
 
-        $scope.showWebcam = function () {
+        $scope.showWebcam = function() {
             $scope.dataUri = null;
             $scope.fromLink = null;
             window.Webcam.set({
@@ -221,55 +221,58 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             window.Webcam.attach('#videoCapture');
         };
 
-        $scope.listSelfies = function () {
+        $scope.listSelfies = function() {
             $http.get('/selfie')
-                    .success(function (selfies) {
-                        $scope.selfies = selfies;
-                    })
-                    .error(function () {
+                .success(function(selfies) {
+                    $scope.selfies = selfies;
+                    console.log(selfies)
+                })
+                .error(function() {
 
-                    });
+                });
         };
 
         if ($stateParams.selfieId) {
             $http.get('/selfie/' + $stateParams.selfieId)
-                    .success(function (data) {
-                        $scope.dataUri = data.dataUri;
-                        $scope.fromLink = true;
-                    })
-                    .error(function () {
-                        $scope.showWebcam();
-                    });
+                .success(function(data) {
+                    $scope.dataUri = data.dataUri;
+                    $scope.fromLink = true;
+                })
+                .error(function() {
+                    $scope.showWebcam();
+                });
         }
         else {
             $scope.showWebcam();
-            $scope.listSelfies();
         }
 
-        $scope.snap = function () {
-            window.Webcam.snap(function (data_uri) {
+        $scope.listSelfies();
+
+
+        $scope.snap = function() {
+            window.Webcam.snap(function(data_uri) {
                 $scope.dataUri = data_uri;
             });
         };
 
-        $scope.delete = function () {
+        $scope.delete = function() {
             $scope.dataUri = null;
         };
 
-        $scope.save = function () {
+        $scope.save = function() {
             $http.post('/selfie', {
                 dataUri: $scope.dataUri
             }).
-                    success(function (data, status, headers, config) {
-                        $scope.delete();
-                        $scope.selfieId = data._id;
-                        $scope.link = $scope.domainName + "/#!/" + $scope.selfieId;
-                        $scope.selfies.unshift(data);
-                    }).
-                    error(function (data, status, headers, config) {
-                        $scope.delete();
-                    });
-        }
+            success(function(data, status, headers, config) {
+                $scope.delete();
+                $scope.selfieId = data._id;
+                $scope.link = $scope.domainName + "/#!/" + $scope.selfieId;
+                $scope.selfies.unshift(data);
+            }).
+            error(function(data, status, headers, config) {
+                $scope.delete();
+            });
+        };
     }
 ]);
 'use strict';
